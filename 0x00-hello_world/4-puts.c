7881e6344915362fc367d635c183d96ca7118f2a
PHAECKE <francisamexo@yahoo.com>
2022-06-02 08:51:04 -0700

    hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /** 
- *  main - Entry point
- *
- *  return - Always 0 (Success)
+ *  main - this is the entry 
+ *  Return - Always 0 after compiling
  */ 
 int main(void)
 {
