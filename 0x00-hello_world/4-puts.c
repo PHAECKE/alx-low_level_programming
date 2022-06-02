7b9856509b006792a176daef2e49d285aa66b50b
PHAECKE <francisamexo@yahoo.com>
2022-06-02 08:53:25 -0700

    hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/** 
- *  main - this is the entry 
+/**
+ *  main - this is the entry
  *  Return - Always 0 after compiling
- */ 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
