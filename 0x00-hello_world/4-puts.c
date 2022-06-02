125c04fca4844a4c14ba9c164bbc2454c1ef07eb
PHAECKE <francisamexo@yahoo.com>
2022-06-02 10:37:35 -0700

    q

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - the entry point
+ *Return - is 0 after compiling
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
