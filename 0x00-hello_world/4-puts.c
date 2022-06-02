eac129ca3ed5af81f3814a5fc39d1ecf384f7a7c
PHAECKE <francisamexo@yahoo.com>
2022-06-02 09:34:14 -0700

    hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *  main - this is the entry
- *  Return - Always 0 after compiling
+ *main - the entry point
+ *Return - is 0 after compilation
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like biulding a multilingual puzzle");
+  return (0);
 }
