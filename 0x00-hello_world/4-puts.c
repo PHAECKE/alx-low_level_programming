056093eef5003e9b97ba3185a7bda1c025e90cd9
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-02 15:18:52 +0100

    hello world C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/* 
+ *  main is the entry point
+ *
+ *  return is always 0 after compiling
+ */ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main is the entry point
+ *
+ * return is always 0 after compiling
+ */
+int main(void)
+{
+	printf("with proper grammar, but outcome is a piece of art,\n");
+	return(0);
+}
