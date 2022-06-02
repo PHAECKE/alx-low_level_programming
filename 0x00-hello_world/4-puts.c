52a7b08a97f4a2a1b06dd7e58d930cde6fce3e19
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-02 17:59:20 +0100

    hello world C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - the entry point
  * Return - is 0 after compiling
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
