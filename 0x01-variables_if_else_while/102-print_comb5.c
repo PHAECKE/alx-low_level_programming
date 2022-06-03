84c18ec2ba8c792c140cbe9bca46bb39d6a3a10b
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-03 20:46:51 +0100

    if else var

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (a = 0; a <= 98; a++)
 	{
-		for (b = a+ 1; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
@@ -20,7 +20,7 @@ int main(void)
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
-			if (a == 98 && b ==99)
+			if (a == 98 && b == 99)
 				continue;
 
 			putchar(',');
