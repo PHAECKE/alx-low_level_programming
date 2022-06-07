7e9dd0a95a4223a09950880cd436efd771ffedc1
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-07 16:17:32 +0100

    nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,18 @@
-#include <>
+#include <stdio.h>
+
+/**
+ * main - is the entry
+ * Return: is 0
+ */
+int main(void)
+{
+	int , b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3#=) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry
+ * Return: is 0
+ */
+int main(void)
+{
+	int a;
+	lont int b, c, d;
+
+	b = 1;
+	c = 2;
+	printf("%ld, %ld", b, c);
+	for (a = 0; a < 48; a++)
+	{
+		d = b + c;
+		printf(", %dld", d);
+		b = c;
+		c = d;
+	}
+	printf("\n");
+	return (0);
+}
