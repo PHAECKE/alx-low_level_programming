acd8648ac39aace2e9f9147a74a9e9c003d5344b
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-07 17:15:11 +0100

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include "main.h"
  /**
   * print_sign - is the entry
-  * Return: os 1 if mumber is positive, 0 if number is 0, or -1 if number is negative
+  * @n: is an integer
+  * Return: os 1 if mumber is positive, 0 if number is 0,
+  * or -1 if number is negative
   */
 int print_sign(int n)
 {
