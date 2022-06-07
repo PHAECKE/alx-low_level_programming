4ecc1c18e062de90be1c18147ab05044d69d9e89
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-07 17:13:06 +0100

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,8 @@
 /**
  * _isalpha - is the entry
  * @c: a character in ASCII code
- * Return: is 1 if output is a letter, lowercase or uppercase and 0 if otherwise
+ * Return: is 1 if output is a letter, lowercase
+ * or uppercase and 0 if otherwise
  */
 int _isalpha(int c)
 {
