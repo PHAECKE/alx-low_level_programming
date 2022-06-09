1ca52e666a0fda2f44c6a5c52ca1bf561c19ec66
PHAECKE <francisamexo@yahoo.com>
2022-06-09 12:10:17 -0700

    more loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,11 +10,11 @@ void print_triangle(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < (size -1); j++)
+		for (j = 1; j < (size - 1); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
 			_putchar('#');
-		if (i < (size -1))
+		if (i < (size - 1))
 			_putchar('\n');
 	}
 	_putchar('\n');
