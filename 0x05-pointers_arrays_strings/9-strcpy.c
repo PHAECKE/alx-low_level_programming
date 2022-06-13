cf8a15c023073ae7b0189d9edb529654a233b596
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-13 15:56:46 +0100

    string arrays pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,9 +10,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *a = dest;
+	int i = -1;
 
-	while (*src)
-		*dest++ = *src++;
-	return (a);
+	do 
+	{
+		i++;
+		dest[i] = src[i];
+	}
+	while (src[i] != '\0');
+	return (dest);
 }
