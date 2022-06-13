345420840db3f647243ad4b925af0e951843f533
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-13 19:46:55 +0100

    string arrays pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,15 +7,21 @@
 */
 int main(void)
 {
-int sum;
-char c;
+	char str[100];
+	int i = 0, a = 0, b = 0;
 
-srand(time(NULL));
-while (sum <= 2645)
-{
-c = rand() % 128;
-sum += c;
-}
-putchar(2772 - sum);
-return (0);
+	srand(time(NULL));
+
+	for (i = 0; c <= 2644; i++)
+	{
+		b = (rand() % 25) + 65;
+		str[i] = b;
+		c = c + b;
+	}
+
+	str[i++] = 2772 - c;
+	str[i++] = '\0';
+
+	printf("%s\n", str);
+	return (0);
 }
