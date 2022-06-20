954cb46c1e421ee9d63d8d542dd49ef192ec41a8
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-06-20 16:42:33 +0100

    pointer arrays

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,6 +4,7 @@
  * @haystack: an input string to search in
  * @needle: an input string to locate into string haystack
  * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -23,5 +24,5 @@ char *_strstr(char *haystack, char *needle)
 			return (haystack);
 		haystack = startj + 1;
 	}
-	return ('\0');
+	return (NULL);
 }
