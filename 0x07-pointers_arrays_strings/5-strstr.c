b63e7ee84c9227d01d1cfee11a346225845d568a
PHAECKE <francisamexo@yahoo.com>
2022-06-20 09:39:22 -0700

    pointer arrays

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
  * _strstr - a function that locates a substring
  * @haystack: an input string to search in
  * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
+ * Return: a pointer to the beginning of the located substring,
  */
 char *_strstr(char *haystack, char *needle)
 {
