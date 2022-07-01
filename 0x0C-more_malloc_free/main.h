888f056ebc9e654e43081875b27e26e6da15f053
PHAECKE <105058306+PHAECKE@users.noreply.github.com>
2022-07-01 16:48:08 +0100

    ...

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int num1[2000], num2[2000], mul[4000] = {0};
+int l1, l2, i, j, tmp, chkk;
+int _strlen(char *s);
