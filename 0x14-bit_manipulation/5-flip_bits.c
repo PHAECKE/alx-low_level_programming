d68ae379744cde0acc5405c502d8065a6b0f4764
PHAECKE <francisamexo@yahoo.com>
2022-07-21 07:39:20 -0700

    ..

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	for (i = 0; i < j; i++)
 	{
 		if ((m & 1) != (n & 1))
-			nflips += 1;
+			flip_num += 1;
 		n = n >> 1;
 		m = m >> 1;
 	}
