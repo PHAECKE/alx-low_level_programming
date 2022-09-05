7894942cf0529d089e0d61d180cffb0075b55a1f
PHAECKE <francisamexo@yahoo.com>
2022-09-05 16:08:10 -0700

    ..

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -shared -o libruin.so
-LD_PRELOAD=$PWD/libruin.so
+wget -P .. https://raw.githubusercontent.com/PHAECKE/alx-low_level_programming/master/0x18-dynamic_libraries/libruin.so
+export LD_PRELOAD="$PWD/../libruin.so"
